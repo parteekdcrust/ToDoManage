c86356a786a47e7075950652daf19ec2f1897662
parteekdcrust <pgarg56023@gmail.com>
2023-06-05 11:54:19 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const port = process.env.PORT;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+});
